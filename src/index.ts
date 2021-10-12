67956e8ad02da5d5f06f5304239da0924c929eb3
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-11 23:50:06 -0300

    adicionando rota para arquivos públicos

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import validator from "validator";
-import express from 'express'
+import path from 'path'
+import express, {Request, Response} from 'express'
 import mainRoutes from './routs/index'
 const server = express();
 
+
+server.use(express.static(path.join(__dirname,'../public'))); //arquivos publicos
+
 server.use(mainRoutes);
+
+server.use((req: Request, res: Response)=>{
+        res.status(404).send('Página não encontrada!');
+});
 
 server.listen(80);
 
