f7bf001de8c884bb80bba372c7a3cf0360dcc041
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-11 22:57:37 -0300

    retirando as rotas do módulo principal

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,14 @@
 import validator from "validator";
-import express, {Request,Response} from 'express'
-
+import express from 'express'
+import mainRoutes from './routes/index'
 const server = express();
+
+server.use(mainRoutes);
+
 server.listen(80);
 
-server.get('/', (req:Request,res:Response)=>{
-    res.send("Olá mundo !!");
-
-
-});
-
-server.get('/:Nome', (req:Request,res:Response)=>{
-    let Nome = req.params.Nome;
-    res.send(`Olá, ${Nome}`);
 
 
 
-});
 
-server.get('/Voos/:ida-:volta', (req:Request,res:Response)=>{
-    let {ida,volta} = req.params;
-    res.send(`Destino: ${ida} Origem: ${volta}`);
-
-
-
-});
-console.log(validator.isEmail("Henriquecezar.jp@gmail.com"));
+console.log(validator.isEmail("teste.jp@gmail.com"));
