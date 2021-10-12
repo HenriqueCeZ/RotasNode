f3d64c177c11b477e79b3fe9770280f327c1ffef
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-11 22:38:36 -0300

    Criando rotas e inicializando servidor

---
+++ src/Matematica.ts
@@ -0,0 +1,3 @@
+export function Somar(x: number, y: number): number {
+    return x+y;
+}
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import validator from "validator";
+import express, {Request,Response} from 'express'
+
+const server = express();
+server.listen(80);
+
+server.get('/', (req:Request,res:Response)=>{
+    res.send("Olá mundo !!");
+
+
+});
+
+server.get('/:Nome', (req:Request,res:Response)=>{
+    let Nome = req.params.Nome;
+    res.send(`Olá, ${Nome}`);
+
+
+
+});
+
+server.get('/Voos/:ida-:volta', (req:Request,res:Response)=>{
+    let {ida,volta} = req.params;
+    res.send(`Destino: ${ida} Origem: ${volta}`);
+
+
+
+});
+console.log(validator.isEmail("Henriquecezar.jp@gmail.com"));
